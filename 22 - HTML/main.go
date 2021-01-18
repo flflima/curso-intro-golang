d9c29c8db507c7eb15198be8877309298056c71b
Felipe Lima <flf.felipe@gmail.com>
2021-01-18 12:24:28 -0300

    Rendereizando HTML dinamico com GO

---
+++ 22 - HTML/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	u := usuario{
+		"João",
+		"joao@email.com",
+	}
+
+	templates.ExecuteTemplate(w, "home.html", u)
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", home)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
